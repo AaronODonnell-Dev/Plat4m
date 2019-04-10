3370df46611871f9b37100000d2746a3b7694d27
Aaron O'Donnell <s00171335@mail.itsligo.ie>
2019-04-10 12:07:42 +0100

    Player Capsule Movement

--- Plat4m/Assets/Aaron/Scripts/PlayerController.cs
+++ Plat4m/Assets/Aaron/Scripts/PlayerController.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody _body;
+
     void Start()
     {
-        
+        _body = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _body.velocity += movement * speed;
     }
 }
