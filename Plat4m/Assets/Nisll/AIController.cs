c56aab04c4c8a6db83b67426ef20991241bddde4
Niall Connolly <S00169104@mail.itsligo.ie>
2019-04-10 15:43:11 +0100

    Scene And AI
    Added a character that follows nodes on a nav-mesh

---
+++ Plat4m/Assets/Nisll/AIController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Nodes))]
+public class AIController : MonoBehaviour
+{
+    public GameObject ObjectToMoveTo;
+    private Nodes nodes;
+
+    void Start()
+    {
+        nodes = GetComponent<Nodes>();
+    }
+
+    bool canMove = true;
+    void Update()
+    {
+        if (canMove)
+            nodes.MoveTo(ObjectToMoveTo);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            nodes.Stop();
+            canMove = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canMove = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Node")
+        {
+            var node = other.gameObject.GetComponent<PathNode>();
+
+            if (node.NextNode != null)
+                ObjectToMoveTo = node.NextNode.gameObject;
+            else
+                canMove = false;
+        }
+    }
+}
