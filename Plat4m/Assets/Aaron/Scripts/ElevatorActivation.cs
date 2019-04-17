5899ab403c272728fef3a61ee5f8d5b9b2423b4c
Aaron O'Donnell <s00171335@mail.itsligo.ie>
2019-04-17 15:37:12 +0100

    Moving Platform Animation, Trigger error, Player collision with Platform

---
+++ Plat4m/Assets/Aaron/Scripts/ElevatorActivation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorActivation : MonoBehaviour
+{
+    public RaycastHit hit;
+    private bool activateLever = false;
+    public GameObject Player;
+    public GameObject elevator;
+
+    public Animation move;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var heading = Player.transform.position - this.transform.position;
+
+        var distance = heading.magnitude;
+        var direction = heading / distance; // This is now the normalized direction.
+
+        if (heading.sqrMagnitude < 7 * 7 && Input.GetKeyDown(KeyCode.E))
+        {
+            activateLever = !activateLever;
+        }
+
+        EnableAnimation();
+    }
+
+    void EnableAnimation()
+    {
+        if (activateLever)
+        {
+            move.Play("Elevator");
+        }
+        else if (!activateLever)
+        {
+            move.Stop("Elevator");
+        }
+    }
+}
