cc71c2d281f139d19c33488269f1ffbc18804a0c
Niall Connolly <S00169104@mail.itsligo.ie>
2019-04-19 00:09:31 +0100

    Ai improvements
    Got the enemy to move from the node and if he sees us move towards us instead using a finite state machine.

---
+++ Plat4m/Assets/Nisll/EasyAI.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class EasyAI : MonoBehaviour
+    {
+        public NavMeshAgent agent;
+        public ThirdPersonCharacter character;
+        //public GameObject character; 
+
+        public enum State
+        {
+            PATROL,
+            CHASE
+        }
+
+        public State state;
+        private bool alive;
+
+        public GameObject[] waypoints;
+        private int waypointInd = 0;
+        public float patrolSpeed = 1.5f;
+
+        public float chaseSpeed = 3f;
+        public GameObject target;
+
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+            agent.updatePosition = true;
+            agent.updateRotation = false;
+
+            state = EasyAI.State.PATROL;
+
+            alive = true;
+
+            //StartCoroutine(FSM());
+        }
+
+        private void Update()
+        {
+            StartCoroutine(FSM());
+        }
+
+        IEnumerator FSM()
+        {
+            while (alive)
+            {
+                switch (state)
+                {
+                    case State.PATROL:
+                        Patrol();
+                        break;
+                    case State.CHASE:
+                        Chase();
+                        break;
+                    default:
+                        break;
+                }
+                yield return null;
+            }
+        }
+
+        void Patrol()
+        {
+            agent.speed = patrolSpeed;
+            if (Vector3.Distance(this.transform.position, waypoints[waypointInd].transform.position) >= 2)
+            {
+                agent.SetDestination(waypoints[waypointInd].transform.position);
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else if (Vector3.Distance(this.transform.position, waypoints[waypointInd].transform.position) <= 2)
+            {
+                waypointInd += 1;
+                if (waypointInd > waypoints.Length)
+                {
+                    waypointInd = 0;
+                }
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+
+        void Chase()
+        {
+            agent.speed = chaseSpeed;
+            agent.SetDestination(target.transform.position);
+            character.Move(agent.desiredVelocity, false, false);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                state = EasyAI.State.CHASE;
+                target = other.gameObject;
+            }
+        }
+    }
+}
