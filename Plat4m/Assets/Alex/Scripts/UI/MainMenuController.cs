5b86b81aedacfa13e90ed0eb0f4a4d1d568764f8
Alex Schell <ASchell16@outlook.com>
2019-04-28 16:24:40 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Plat4m/Assets/Alex/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : UIController
+{  
+  
+    void Update()
+    {
+       
+    }
+
+    public void Start_OnClick()
+    {
+        mainMenu.SetActive(false);
+        Camera.main.transform.LookAt(player.transform);       
+        Camera.main.transform.RotateAround(player.transform.position, Vector3.up, 180f);       
+    }
+    public void Quit_OnClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+
+}
---
+++ Plat4m/Assets/Alex/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuController : UIController
+{
+    private void Start()    {
+    }
+    void Update()
+    { }
+    // When the Resume Button is Pressed
+    public void Resume_OnClick()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    // When the Options Button is Pressed
+    public void Options_OnClick()
+    {
+     
+    }
+    // When the Save Button is Pressed
+    public void Save_OnClick()
+    {
+       // BinaryFormatter bf = new BinaryFormatter();
+       // FileStream file = File.Open(Application.persistentDataPath + "/PlayerInfo.dat", FileMode.Open);
+
+
+        //bf.Serialize(file,  );
+    }
+    // When the Quit Button is pressed
+    public void Quit_OnClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
