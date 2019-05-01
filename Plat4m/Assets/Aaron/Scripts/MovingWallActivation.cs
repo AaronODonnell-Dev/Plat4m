d10ed8a08f94e9e650b92b3ea21a51130624cbe3
Aaron O'Donnell <s00171335@mail.itsligo.ie>
2019-05-01 13:23:35 +0100

    Updating commit
    Moving Wall Added, limited functionality

---
+++ Plat4m/Assets/Aaron/Scripts/MovingWallActivation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWallActivation : MonoBehaviour
+{
+    public RaycastHit hit;
+    private bool activateLever = false;
+    public GameObject Player;
+    public GameObject movingWall;
+    public GameObject instructionMessage;
+
+    public Animation move;
+
+    private void Start()
+    {
+        move.GetComponent<Animation>();
+        move["MovingWall"].speed = 0.15f;
+    }
+
+    void Update()
+    {
+        var heading = Player.transform.position - this.transform.position;
+
+        var distance = heading.magnitude;
+        var direction = heading / distance; // This is now the normalized direction.
+
+        if (heading.sqrMagnitude < 7 * 7)
+        {
+            instructionMessage.SetActive(true);
+
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                activateLever = !activateLever;
+                instructionMessage.SetActive(false);
+            }
+        }
+
+        EnableAnimation();
+    }
+
+    void EnableAnimation()
+    {
+        if (activateLever)
+        {
+            move.Play("MovingWall");
+            
+        }
+        else if (!activateLever)
+        {
+            move.Stop("MovingWall");
+        }
+    }
+}
