527f1b4afd9db3127ce3db8d3d5caed2ed2aa884
Aaron O'Donnell <s00171335@mail.itsligo.ie>
2019-05-28 16:50:58 +0100

    Yeet nearly ready
    fixed time freeze and arrow not working at all

--- Plat4m/Assets/Aaron/Scripts/Yeet.cs
+++ Plat4m/Assets/Aaron/Scripts/Yeet.cs
@@ -7,83 +7,70 @@ public class Yeet : MonoBehaviour
     Vector3 direction;
     public GameObject Player2;
     PlayerMovement playerMovement;
-    public float speed = 20f;
-    public float maxtime = 1f;
     public Transform arrow;
-    public GameObject effect;
-    float YeetForce;
+    float YeetForce = 17;
     Animation arrowMove;
-    public bool wasYeeted;
-    public bool canYeet;
+    public bool isYeeting;
 
     // Use this for initialization
     void Start()
     {
         playerMovement = new PlayerMovement();
-        arrowMove = arrow.GetComponent<Animation>();
-        arrowMove["YeetArrow"].speed = 0.15f;
+        isYeeting = false;
         arrow.gameObject.SetActive(false);
-
-        YeetForce = 100 * 17;
-    }
-
-
-    IEnumerator Counter()
-    {
-        //pauses time 
-        float stopTime = Time.realtimeSinceStartup + maxtime;
-
-        while (Time.realtimeSinceStartup < stopTime)
-        {
-            yield return null;
-        }
-
-        if (Time.timeScale == 0)
-        {
-            Time.timeScale = 1f;
-            canYeet = false;
-            arrow.gameObject.SetActive(false);
-        }
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        var heading = transform.position - Player2.transform.position;
-
-        var distance = heading.magnitude;
-        var direction = heading / distance; // This is now the normalized direction.
-
-        StartCoroutine("Counter");
-
-        Time.timeScale = 0;
-
-        canYeet = true;
-        arrow.position = transform.position + (Camera.main.transform.forward * 2);
-        //arrow.Translate(0, 0, 10);
-
-        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        diff.Normalize();
-
-        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        arrow.transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
-        arrowMove.cullingType = AnimationCullingType.BasedOnRenderers;
-        //Instantiate(effect, transform.position, Quaternion.identity);
-
-        if (heading.sqrMagnitude < 3 * 3)
+        if (Input.GetKey(KeyCode.Y))
         {
-            arrow.gameObject.SetActive(true);
+            possibleYeets();
+        }
+        else if(Input.GetKeyUp(KeyCode.Y) && isYeeting)
+        {
+            Throw();
+        }
+        
+    }
 
-            if(Input.GetKeyDown(KeyCode.Y))
+    public void Throw()
+    {
+        GetComponent<Rigidbody>().AddForce(-direction * YeetForce * 85, ForceMode.Force);
+
+        isYeeting = false;
+        arrow.gameObject.SetActive(false);
+
+        Time.timeScale = 1;
+    }
+
+    public void possibleYeets()
+    {
+        if (!isYeeting)
+        {
+            var heading = transform.position - Player2.transform.position;
+
+            var distance = heading.magnitude;
+            var direction = heading / distance; // This is now the normalized direction.
+
+            if (heading.sqrMagnitude < 3 * 3)
             {
-                transform.GetComponent<Rigidbody>().AddForce(-arrow.transform.forward + Vector3.up * YeetForce, ForceMode.Force);
-                arrow.gameObject.SetActive(false);
-                playerMovement.jumpLimit--;
-                wasYeeted = true;
+                isYeeting = true;
+                arrow.gameObject.SetActive(true);
+                Time.timeScale = 0;
             }
         }
-            //break;
+        else CalculateDirection();
+    }
+
+    public void CalculateDirection()
+    {
+        direction = - Vector3.up;
+
+        float rot_z = 180;
+
+        arrow.transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
     }
 
     //void OnDrawGizmos()
