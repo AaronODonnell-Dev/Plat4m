c2ceb84fb09151a4a49a2e6d8696fc0cc1558cb7
Niall Connolly <S00169104@mail.itsligo.ie>
2019-08-13 16:35:11 +0100

    Auto stash before merge of "master" and "origin/master"

--- Plat4m/Assets/Aaron/Scripts/PlayerHealth.cs
+++ Plat4m/Assets/Aaron/Scripts/PlayerHealth.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -88,3 +89,114 @@ public class PlayerHealth : MonoBehaviour
         playerMovement.enabled = false;
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    Animator anim;
+    AudioSource playerAudio;
+    Movement playerMovement;
+    bool isDead;
+    bool damaged;
+
+    
+    void Awake()
+    {
+        playerMovement = GetComponent<Movement>();
+
+        // Set the initial health of the player.
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+
+        // If the player has just been damaged...
+        if (damaged)
+        {
+            // ... set the colour of the damageImage to the flash colour.
+            damageImage.color = flashColour;
+        }
+        // Otherwise...
+        else
+        {
+            // ... transition the colour back to clear.
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        // Reset the damaged flag.
+        damaged = false;
+    }
+
+    #region Collision
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log("Entered");
+    //}
+
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    Debug.Log("Exit");
+    //}
+
+    //void OnCollisionStay(Collision collision)
+    //{
+    //    Debug.Log("Damage");
+    //    Destroy(gameObject);
+    //    //TakeDamage(1);
+    //}
+    #endregion
+
+    public void TakeDamage(int amount)
+    {
+        // Set the damaged flag so the screen will flash.
+        damaged = true;
+
+        // Reduce the current health by the damage amount.
+        currentHealth -= amount;
+
+        // Set the health bar's value to the current health.
+        healthSlider.value = currentHealth;
+
+        // Play the hurt sound effect.
+        //playerAudio.Play();
+
+        // If the player has lost all it's health and the death flag hasn't been set yet...
+        if (currentHealth <= 0 && !isDead)
+        {
+            // ... it should die.
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        // Set the death flag so this function won't be called again.
+        isDead = true;
+
+        Debug.Log("Dead");
+        // Tell the animator that the player is dead.
+        //anim.SetTrigger("Die");
+
+        // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
+        //playerAudio.clip = deathClip;
+        //playerAudio.Play();
+
+        // Turn off the movement and shooting scripts.
+        playerMovement.enabled = false;
+    }
+}
+>>>>>>> Stashed changes
